210c92dbdce5bc74d7780c791424da3e2f3143df
Irene Ros <irene@bocoup.com>
2012-03-27 14:14:24 -0400

    added flot piechart rendering

--- src/snippets/dataset/examples/flotandtwitter.js
+++ src/snippets/dataset/examples/flotandtwitter.js
@@ -1,6 +1,5 @@
 // query twitter for tweets containing the term "javascript"
-// and get 100 per page. Note that twitter uses jsonp requests, 
-// so we toggle that flag too.
+// and get 100 per page. 
 var ds = new Miso.Dataset({
   url : "http://search.twitter.com/search.json?q=javascript&rpp=100&callback=",
   interval : 3000,
@@ -10,9 +9,11 @@ var ds = new Miso.Dataset({
   // we only actually want the number of urls
   extract : function(data) {
     
+    // add some properties to the tweets like the number of urls they have
+    // and whether they have urls at all.
     _.each(data.results, function(tweet){
       tweet.urlCount = Math.max(tweet.text.split("http").length-1, 0);
-      tweet.hasUrls  = (tweet.urlCount > 0);
+      tweet.hasUrls  = (tweet.urlCount > 0) ? "Has Urls" : "Has No Urls";
     });
 
     return data.results;
@@ -24,13 +25,61 @@ var urlRatioDataset = null;
 ds.fetch({
   success : function() {       
     if (urlRatioDataset === null) {
+      // compute a group by that aggregates the number of tweets that
+      // do and don't have urls.
       urlRatioDataset = this.groupBy("hasUrls", ["urlCount"], { 
         method : function(arr) { 
           return arr.length; 
         }
       });
+
+      var paintPieChart = function() {
+        var series = [];
+        
+
+        // convert data to flot format:
+        // { label : "", data : N }
+        urlRatioDataset.each(function(row) {
+          series.push({ label : row.hasUrls, data : row.urlCount });
+        });
+        
+        console.log(series);
+
+        // create a pie chart.
+        // For more info see:
+        // http://people.iola.dk/olau/flot/examples/pie.html
+        $.plot($("#pieContainer"), 
+          series,
+          {  
+            legend: {
+              show: false
+            },
+            series: {
+              pie: { 
+                show: true,
+                label: {
+                  show: true,
+                  radius: 1,
+                  formatter: function(label, series){
+                    return '<div style="font-size:10pt;text-align:center;padding:5px;color:black;">'+
+                    label+
+                    '<br/>'+Math.round(series.percent) +
+                    '% ('+ series.datapoints.points[1] + ')</div>';
+                  },
+                  background: { opacity: 0.8 }
+                }
+              }
+            }
+          });
+      };
+
+      // subscribe to changes to the group by.
+      urlRatioDataset.bind("change", paintPieChart);
+
+      // render the chart the first time. Subsequent updates will go through
+      // the change event.
+      paintPieChart();
     }
-    console.log(urlRatioDataset.syncable);
-    console.log(urlRatioDataset.column("urlCount").data);
+    
   }
 });
