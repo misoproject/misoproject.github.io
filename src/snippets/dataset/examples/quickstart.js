a07524b04697366ec6a53c809b84952810d650a5
Irene Ros <irene@bocoup.com>
2012-04-04 21:40:20 -0400

    Niftier quick start example

--- src/snippets/dataset/examples/quickstart.js
+++ src/snippets/dataset/examples/quickstart.js
@@ -1,15 +1,28 @@
-//Load Uranium Production Data
+//Load Uranium CSV File. Looks like:
+// year,value
+// 1980,43.7
+// 1981,38.47
+// ...
+// 2005,2.69
 var ds = new Miso.Dataset({
   url : "/data/uranium.csv",
   delimiter : ",",
   columns : [
-    { name : "Year", type : "time", format : "YYYY" }
+    { name : "year", type : "time", format : "YYYY" }
   ]
 });
 
-//Select the rows in the '80s
-ds.each(function(row) {
-  log(JSON.stringify(row));
-});
+ds.fetch({
+  success: function() {
+    //Select the rows in the '80s
+    var uraniumInThe80s = this.where({ 
+      rows : function(row) {
+        return (row.year >= moment("1980") && 
+                row.year < moment("1990"));
+      }
+    });
 
-//Find their average
+    // output their average
+    log("Average:", uraniumInThe80s.mean("value"));
+  }
+});
