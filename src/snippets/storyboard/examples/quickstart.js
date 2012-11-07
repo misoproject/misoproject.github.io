cd050a3a06e8df4e3e9c4522612838daba573b59
Alex Graul <alex.graul@guardian.co.uk>
2012-11-07 16:26:18 +0000

    quickstart tidyup and text tweaks

--- src/snippets/storyboard/examples/quickstart.js
+++ src/snippets/storyboard/examples/quickstart.js
@@ -11,11 +11,14 @@ var boxes = new Miso.Storyboard({
       },
 
       exit : function() {
-        // we are almost done loading, set up the stage
-        // with the elements we need
+        // we are almost done loading,
+        // set up the stage
+        // with the elements we need.
         demoBlock.empty();
         for(var i = 0; i < 9; i++) {
-          var box = $('<div>', { className : 'demoBox' }).css({
+          var box = $('<div>')
+            .attr('class', 'demoBox')
+            .css({
               display : 'none',
               width : '50px',
               height : '50px',
@@ -33,35 +36,34 @@ var boxes = new Miso.Storyboard({
 
     painting : {
       enter : function() {
-        // we are going to use some jQuery transitions, so this will be
+        // we are going to use some jQuery 
+        // transitions, so this will be
         // an async function
         var done = this.async();
         
         // fade the boxes in
-        for(var i = 0; i < boxCache.length; i++) {
-          boxCache[i].delay(i * 200).fadeIn('slow');
-          // on the last box, make sure you notify that we are done with
-          // this specific sequence by calling the done function!
+        _.each(boxCache, function(box, i) {
+          box.delay( i * 200 ).fadeIn('slow');
+
+          // on the last box, make sure you notify 
+          // that we are done with this specific 
+          // sequence by calling the done function!
           if (i === boxCache.length-1) {
-            boxCache[i].promise().done(function() {
-              done();
-            })
+            boxCache[i].promise().done( done );
           }
-        }
+        });
       },
 
       exit : function() {
         var done = this.async();
-        
-        // fade the boxes in
-        for(var i = boxCache.length - 1; i >= 0; i--) {
-          boxCache[i].delay((boxCache.length - i + 1) * 400).fadeOut('slow');
+        _.each(boxCache, function(box, i) {
+          var delay = (boxCache.length-i+1)*400
+          box.delay( delay ).fadeOut('slow');
           if (i === 0) {
-            boxCache[i].promise().done(function() {
-              done();
-            })
+            boxCache[i].promise().done( done );
           }
-        }
+
+        });
       }
     },
 
